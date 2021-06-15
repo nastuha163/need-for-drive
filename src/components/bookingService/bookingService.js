083ac6cfe8cb8bdb001229cf75e5930b7b331146
Nastya <logistika94@mail.ru>
2021-06-15 13:29:36 +0400

    booking-service

--- src/components/bookingService/bookingService.js
+++ src/components/bookingService/bookingService.js
@@ -1,69 +1,136 @@
 
 import './bookingService.scss';
-import React from 'react';
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import BurgerMenu from '../burgerMenu';
 import mapSvg from '../../assets/icons/map.svg';
 import Additionally from './additionally';
 import Location from './location';
 import ModelCars from './modelCars';
 import Result from './result';
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
-const BookingService = () => {
 
-    const Header = () => {
-        return (
-            <>
-                <div className='header-block'>
-                    <Link to='/' className='header-name'>Need for drive</Link>
-                    <div type='button' className='map-booking-service'>
-                        <img className='map-page' src={mapSvg} alt="map img" />
-                        <div className='map-name'>Ульяновск</div>
-                    </div>
-                </div>
-                <div>
-                    <div className='buttons-booking-service'>
-                        <div className='button-booking-service' type='button'>Местоположение</div>
-                        <div className='pointer' />
-                        <Link to='/booking/location/modal' className='button-booking-service'>Модель</Link>
-                        <div className='pointer' />
-                        <Link to='/booking/location/modal/additionally' className='button-booking-service'>Дополнительно</Link>
-                        <div className='pointer' />
-                        <Link to='/booking/location/modal/additionally/result' className='button-booking-service'>Итого</Link>
-                    </div>
-                </div>
-            </>
-        )
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
+
+
+const Tabs = () => {
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = tab => {
+        if (activeTab !== tab) setActiveTab(tab);
     }
+
     return (
-        <Router basename="/need-for-drive/" >
+        <div className='buttons-booking-service'>
+            <Nav tabs className='button-booking-service'>
+                <NavItem>
+                    <NavLink type='button'
+                        className={classnames({ active: activeTab === '1' })}
+                        onClick={() => { toggle('1'); }}
+                    >Местоположение
+            </NavLink>
+                </NavItem>
+                <div className='pointer' />
+                <NavItem>
+                    <NavLink type='button'
+                        className={classnames({ active: activeTab === '2' })}
+                        onClick={() => { toggle('2'); }}
+                    >Модель
+            </NavLink>
+                </NavItem>
+                <div className='pointer' />
+                <NavItem>
+                    <NavLink type='button'
+                        className={classnames({ active: activeTab === '3' })}
+                        onClick={() => { toggle('3'); }}
+                    >Дополнительно
+            </NavLink>
+                </NavItem>
+                <div className='pointer' />
+                <NavItem>
+                    <NavLink type='button'
+                        className={classnames({ active: activeTab === '4' })}
+                        onClick={() => { toggle('4'); }}
+                    >Итого
+            </NavLink>
+                </NavItem>
+            </Nav>
+
+            <TabContent className='custom-part' activeTab={activeTab}>
+                <TabPane tabId="1">
+                    <Row>
+                        <Col sm="12">
+                            <Location />
+                        </Col>
+                    </Row>
+                </TabPane>
+                <TabPane tabId="2">
+                    <Row>
+                        <Col sm="6">
+                            <ModelCars />
+                        </Col>
+                    </Row>
+                </TabPane>
+                <TabPane tabId="3">
+                    <Row>
+                        <Col sm="6">
+                            <Additionally />
+                        </Col>
+                    </Row>
+                </TabPane>
+                <TabPane tabId="4">
+                    <Row>
+                        <Col sm="6">
+                            <Result />
+                        </Col>
+                    </Row>
+                </TabPane>
+            </TabContent>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div className='header-block'>
+            <Link to='/' className='header-name'>Need for drive</Link>
+            <div type='button' className='map-booking-service'>
+                <img className='map-page' src={mapSvg} alt="map img" />
+                <div className='map-name'>Ульяновск</div>
+            </div>
+        </div>
+    )
+}
+
+const Order = () => {
+    return (
+        <div className='order'>
+            <div className='order-header'> Ваш заказ: </div>
+            <div className='order-list'>
+                <div>Пункт выдачи:</div>
+                <div>Адрес:</div>
+            </div>
+            <button className='order-button'>Выбрать модель</button>
+        </div>
+    )
+}
+const BookingService = () => {
+    return (
+        <>
             <div className='wrapper-booking-service'>
                 <div className='left-menu-booking-service'>
-                    <BurgerMenu />
+                    <BurgerMenu/>
                 </div>
                 <div className='right-menu-booking-service'>
-                    <Header />
+                    <Header/>
                     <div className='booking-block'>
-                        <div className='custom-part'>
-                            <Location />
-                            <Route path='/booking/location/modal/' exact component={ModelCars} />
-                            <Route path='/booking/location/modal/additionally/' exact component={Additionally} />
-                            <Route path='/booking/location/modal/additionally/result' exact component={Result} />
-                            <Redirect to='/' />
-                        </div>
-                        <div className='order'>
-                            <div className='order-header'> Ваш заказ: </div>
-                            <div className='order-list'>
-                                <div>Пункт выдачи:</div>
-                                <div>Адрес:</div>
-                            </div>
-                            <button className='order-button'>Выбрать модель</button>
-                        </div>
+                        <Tabs/>
+                        <Order/>
                     </div>
                 </div>
             </div>
-        </Router>
+        </>
     )
 }
 
--- src/components/bookingService/location/location.js
+++ src/components/bookingService/location/location.js
@@ -1,33 +1,61 @@
 import React from 'react';
 import './location.scss';
-import img1 from './Rectangle.png';
+// import img1 from './Rectangle.png';
+
+// const address = [
+//     { id: 1, street: 'Ульяновк, Димитровградское ш., 8к', },
+//     { id: 1, street: 'Ульяновк, Октябрьская ул., 22Л', },
+//     { id: 2, street: 'Самара, Московское ш, 264', },
+//     { id: 2, street: 'Самара, Заводское ш., 14, корп. 1', },
+//     { id: 2, street: 'Самара, Алма-Атинская ул., 87', },
+//     { id: 3, street: 'Казань, Московская ул., 20', },
+//     { id: 3, street: 'Казань, Даурская ул., 18', },
+//     { id: 4, street: 'Саранск, Строительная ул., 1Е', }
+// ];
+
+// const SelectAddress = address.map((id, street) => {
+//     return(
+//         <datalist>
+//         <option id={id} value={street} />
+//         </datalist>
+//     )
+
+// });
 
 const Location = () => {
 
     return (
         <>
             <div className="wrapper-location">
-                <form >
+                <div>
                     <label className='header-location-text'>Город</label>
-                    <input
-                        className='list-location'
+                    <input className='list-location'
+                        list='select-city'
                         type="search"
                         placeholder="Начните вводить город..."
-                    // onChange={}
                     />
-                </form>
-                <form>
+                    <datalist id='select-city'>
+                        <option id='1' value="Ульяновск" />
+                        <option id='2' value="Самара" />
+                        <option id='3' value="Казань" />
+                        <option id='4' value="Саранск" />
+                    </datalist>
+                </div>
+                <div>
                     <label className='header-location-text'>Пункт выдачи</label>
-                    <input
-                        className='list-location'
+                    <input className='list-location'
+                        list='select-address'
                         type="search"
                         placeholder="Начните вводить пункт..."
-                    // onChange={} 
+
                     />
-                </form>
+                    <datalist id='select-address'>
+                        {/* <option id={SelectAddress} /> */}
+                    </datalist>
+                </div>
             </div>
             <div className='map-location'>Выбрать на карте:</div>
-            <img src={img1} alt='карта' />
+            {/* <img src={img1} alt='карта' /> */}
         </>
     )
 
