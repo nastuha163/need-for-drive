2d7bfc88564ea311e0a3d1bf007a37b3c4b217c6
Nastya <logistika94@mail.ru>
2021-06-16 15:39:38 +0400

    booking-service-location

--- src/components/bookingService/location/location.js
+++ src/components/bookingService/location/location.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './location.scss';
 // import img1 from './Rectangle.png';
 
-// const address = [
-//     { id: 1, street: 'Ульяновк, Димитровградское ш., 8к', },
-//     { id: 1, street: 'Ульяновк, Октябрьская ул., 22Л', },
-//     { id: 2, street: 'Самара, Московское ш, 264', },
-//     { id: 2, street: 'Самара, Заводское ш., 14, корп. 1', },
-//     { id: 2, street: 'Самара, Алма-Атинская ул., 87', },
-//     { id: 3, street: 'Казань, Московская ул., 20', },
-//     { id: 3, street: 'Казань, Даурская ул., 18', },
-//     { id: 4, street: 'Саранск, Строительная ул., 1Е', }
-// ];
+const address = [
+    { city: 'Ульяновск', street: 'Димитровградское ш., 8к, Ульяновск' },
+    { city: 'Ульяновск', street: 'Октябрьская ул., 22Л, Ульяновск' },
+    { city: 'Самара', street: 'Московское ш, 264, Самара' },
+    { city: 'Самара', street: 'Заводское ш., 14, корп. 1, Самара' },
+    { city: 'Самара', street: 'Алма-Атинская ул., 87, Самара' },
+    { city: 'Казань', street: 'Московская ул., 20, Казань'},
+    { city: 'Казань', street: 'Даурская ул., 18, Казань'},
+    { city: 'Саранск', street: 'Строительная ул., 1Е, Саранск', }
+];
 
-// const SelectAddress = address.map((id, street) => {
-//     return(
-//         <datalist>
-//         <option id={id} value={street} />
-//         </datalist>
-//     )
 
-// });
 
 const Location = () => {
+    const [itemCity, setCity] = useState('');
+    const [itemAddress, setAddress] = useState('');
+    let [ListAddress, setAddressList] = useState([]);
+
+    const setAddressValue = (value) => {
+        setAddress(value.target.value);
+    }
+    const setCityValue = (value) => {
+        setCity(value.target.value)
+        selectAddress(value.target.value);
+        setAddress('');
+    }
+
+    const selectAddress = (itemCity) => {
+        ListAddress = address.filter((itemAddress) => {
+            if (itemCity === itemAddress.city) {
+                return (
+                    itemAddress.street
+                )
+            }
+        });
+        setAddressList(ListAddress);
+        return ListAddress;
+    }
 
     return (
         <>
@@ -33,6 +50,8 @@ const Location = () => {
                         list='select-city'
                         type="search"
                         placeholder="Начните вводить город..."
+                        value={itemCity}
+                        onChange={event => setCityValue(event)}
                     />
                     <datalist id='select-city'>
                         <option id='1' value="Ульяновск" />
@@ -46,15 +65,19 @@ const Location = () => {
                     <input className='list-location'
                         list='select-address'
                         type="search"
+                        value={itemAddress}
+                        onChange={event => setAddressValue(event)}
                         placeholder="Начните вводить пункт..."
 
                     />
                     <datalist id='select-address'>
-                        {/* <option id={SelectAddress} /> */}
+                        {ListAddress.map((item, key) =>
+                            <option key={key} value={item.street} />
+                        )}
                     </datalist>
                 </div>
             </div>
-            <div className='map-location'>Выбрать на карте:</div>
+            <div className='map-location'>Выбрать на карте: {itemCity}</div>
             {/* <img src={img1} alt='карта' /> */}
         </>
     )
